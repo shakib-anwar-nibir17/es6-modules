04b378fc0b56285380c294a5ea9f49082936dfa3
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-09-15 07:33:38 +0600

    memeorable water bottle project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import "./App.css";
-import Watch from "./components/Watch/Watch";
-import { useState, useEffect } from "react";
+import Bottles from "./components/Bottles/Bottles";
+import Header from "./components/Header/Header";
 
 function App() {
-  // const watches = [
-  //   { id: 1, name: "Apple Watch", price: 200 },
-  //   { id: 2, name: "Nova Watch", price: 400 },
-  //   { id: 1, name: "OY Watch", price: 100 },
-  // ];
-
-  const [watches, setWatches] = useState([]);
-  useEffect(() => {
-    fetch("watches.json")
-      .then((res) => res.json())
-      .then((data) => setWatches(data));
-  }, []);
   return (
     <>
-      <h1>Vite + React</h1>
-      {watches.map((watch) => (
-        <Watch key={watches.model} watch={watch}></Watch>
-      ))}
+      <Header></Header>
+      <Bottles></Bottles>
     </>
   );
 }
---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
+
+const Bottles = () => {
+  const [bottles, setBottles] = useState([]);
+  useEffect(() => {
+    fetch("bottle.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+  return (
+    <div>
+      <h3>Bottles Here:{bottles.length}</h3>
+      <div className="bottle-container">
+        {bottles.map((bottle) => (
+          <Bottle key={bottle.id} bottle={bottle}></Bottle>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Bottles;
